21eabbf1c50f1e6df6c91262168dee7a1a6da176
McpePuki <60613217+McpePuki@users.noreply.github.com>
2020-02-09 23:34:46 +0900

    Update Main.php

--- 상점/src/Pmarket/Main.php
+++ 상점/src/Pmarket/Main.php
@@ -177,7 +177,7 @@ class Main extends PluginBase implements Listener {
             $player->sendMessage($this->msg."금액이 부족해서 구매를 하실수 없습니다.");
             $my = EconomyAPI::getInstance()->myMoney();
             $fil = $this->tb['활동'][$name]['구매가'] - $my ;
-            $player->sendMessage($this->msg."부족금액 : {$fil} │ 현재금액 : {$my}); 
+            $player->sendMessage($this->msg."부족금액 : {$fil} │ 현재금액 : {$my}");
             return true;
            }
           $itemname = $this->tb['활동'][$name]['아이템이름'];
